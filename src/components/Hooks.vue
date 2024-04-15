73c6438d18d4b713e8c7f2893693a57ef11cd4a6
Fredin <fredinfu@live.com>
2024-04-14 20:36:10 -0600

    Hooks component init

---
+++ src/components/Hooks.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" placeholder="Ingrese una palabra.." class="textbox" >
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Hooks",
+        data() {
+            return {
+                name: ""
+            }
+        }
+    }
+</script>
+    
+<style>
+    .textbox{
+        display: grid;
+        grid-template-rows: auto;
+        justify-items: center;
+        align-items: center;
+        margin: 0 auto 2rem;
+    }
+</style>
